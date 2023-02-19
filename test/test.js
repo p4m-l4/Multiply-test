86e0e9b118d5efe0eb7edb59b9e21c861d9edb8b
Pamela Namala <namala.pamela@gmail.com>
2023-02-19 10:37:23 +0300

    TDD test cycle 2 - failed/ red

--- test/test.js
+++ test/test.js
@@ -7,4 +7,8 @@ describe('Testing multiplication function', () => {
   it('should return 1', () => {
         assert.equal(multiply(1, 1), 1);
     });
+
+    it('Testing 2 by 2 to return 4', () => {
+        assert.equal(multiply(2, 2), 4);
+    });
   });
