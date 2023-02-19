a1f4c3dcd78ebdbda40e386be47f6e2d3e713bb1
Pamela Namala <namala.pamela@gmail.com>
2023-02-19 10:42:53 +0300

    TDD test cycle 5 - passed/ green

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Testing multiplication function', () => {
     it('Testing 2 by 2 to return 4', () => {
         assert.equal(multiply(2, 2), 4);
     });
-    
+
     it('Testing 3x3 to return 9', () => {
         assert.equal(multiply(3, 3), 9);
     });
@@ -20,7 +20,11 @@ describe('Testing multiplication function', () => {
         assert.equal(multiply(4, 4), 16);
     });
 
-    it('Testing 4x4 to return 16', () => {
-        assert.equal(multiply(4, 4), 16);
+    it('Testing 23x45 to return 23*45', () => {
+        assert.equal(multiply(23, 45), 23 * 45);
+    });
+
+    it('Testing -23x45 to return 23*45', () => {
+        assert.equal(multiply(-23, 45), -23 * 45);
     });
   });
